77724c40a97d04b03b4e5a428df14230ad50df2c
ArkanAli7 <arkan7ali7@gmail.com>
2022-11-26 22:53:49 +0200

    files added

---
+++ HW2/hist.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MIN 0
+#define MAX 100
+
+	
+
+void hist(FILE *f, int nbins){
+	int scanfreturn;
+	int line_number=1;
+	int grade;
+	double pace;
+	int right;
+	int left;
+	int *bins =malloc(sizeof(int)*(nbins+1));
+	while(1){
+		scanfreturn=fscanf(f,"%d",&grade);   /*taking grades from file*/
+		if(scanfreturn == EOF){                  /*check errors*/
+			break;
+		}	
+		else if(scanfreturn!=1){
+			fprintf(stderr,"This is not a number: \%d\"\n",line_number);
+			exit(1);
+		}
+		else if(grade>MAX || grade<MIN){             /*check if grades in range*/
+			fprintf(stderr,"This is not in our range , line number is: \%d\"\n",line_number);
+			exit(1);
+		}
+		                             /*filling the histogram*/
+		int n = grade/(100/ nbins) ;
+		if(n<nbins){
+		bins[n]++;
+		}
+		else if(n == nbins){
+		bins[n-1]++;
+		}
+		line_number++;
+	}
+	 pace=100.0 / nbins; 	 
+	 for(int i=0;i<nbins; i++) {
+	 	left = pace*i;
+	 	if(i<nbins-1 && i==0){
+	 	   	right= ((i+1)*(pace-1));
+	 	 }
+	 	 else if((i>0) && (i<nbins-1)){
+	 		 right= left+pace-1;
+	 	 }
+	 	else if(nbins=100 && i==99){
+	 		right=left;
+	 		fprintf(stdout,"%d-%d	%d\n",left,right,bins[i]);
+	 		fprintf(stdout,"%d-%d	%d\n",left+1,right+1,bins[i+1]);
+	 		break;
+	   	}	
+	 	else right=100; 
+	 	fprintf(stdout,"%d-%d	%d\n",left,right,bins[i]);
+	}
+		free(bins);
+}
+int main(int argc, char**argv){
+	FILE *f;
+	f=stdin;
+	int nbins=10;
+	for(int i=1 ; i<argc-1; i++)
+	{
+	if(!strcmp(argv[i],"-"))       
+	  {
+	  f=stdin;
+	  }
+	  else if(!strcmp(argv[i],"-n_bins"))
+	  {
+	  nbins= i<argc-1 ? atoi(argv[i+1]):10;
+	  i++;
+	  }
+	  else
+	  {
+	  f=fopen(argv[i], "r");
+	  }
+	}
+	
+	if(!f)
+	{
+	fprintf(stderr,"Find not found: \%s\"\n",argv[1]);
+	return 1;
+	}
+	hist(f,nbins);
+	fclose(f);
+	return 0;
+}
---
+++ HW2/median.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MIN 0
+#define MAX 100
+
+void median(FILE* f) {
+	int grade;
+	int hist[100]={0};
+	int linenumber=1;
+	int c=0;
+	int scanfreturn=0;
+	int mediangrade=0;
+	int students=0;
+	int median=0;
+	
+	while(1){
+	scanfreturn=fscanf(f,"%d",&grade);        /*taking grades from file*/
+	if(scanfreturn == EOF){                       /*check errors*/
+		break;
+	}	
+	else if(scanfreturn!=1){
+
+		fprintf(stderr,"This is not a number: \%d\"\n",linenumber);
+		exit(1);
+	}
+	else if(grade>MAX || grade<MIN){         /*check if grades in range*/
+
+		fprintf(stderr,"This is not in our range , line number is: \%d\"\n",linenumber);
+		exit(1);
+		
+	}
+	students++;
+	hist[grade]++;
+	linenumber++;
+	}
+	median= (students+1)/2;
+	for (int i = 0; i < 100; i++) {
+		c += hist[i];
+		if(c>=median){
+			mediangrade=i;
+			break;					
+		}
+	}
+	fprintf(stdout,"%d	",mediangrade);
+
+}
+
+int main(int argc, char**argv){
+	FILE *fd;
+	if(argc == 1 || !strcmp("-",argv[1])){
+		fd=stdin;
+	}	
+	else{
+		fd=fopen(argv[1], "r" );
+	}
+	/*check errors*/
+	if(!fd){
+		fprintf(stderr,"File not found: \"%s\"\n", argv[1]);
+		return 1 ;
+	}
+	median(fd);
+
+	fclose(fd);
+}
